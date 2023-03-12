ba867acc878a8fa037153bf75ce1c1aa85fdef3f
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2023-03-12 13:01:52 -0300

    Função anterior e posterior adicionado

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ let img_p2 = document.getElementById("img_p2");
 let img_p3 = document.getElementById("img_p3");
 let img_p4 = document.getElementById("img_p4");
 let img_fundo = document.querySelector(".produtos");
+let imagens = [img_p1.src, img_p2.src,
+			   img_p3.src, img_p4.src];
+let num = 0;
 
 function TrocarImagem(src, id) {
     img_p1.style.border = "1px solid #999999";
@@ -14,6 +17,18 @@ function TrocarImagem(src, id) {
     img_fundo.src = src;
 }
 
-function TrocarProduto(img) {
+function anterior() {
+    num--;
+    if (num < 0) {
+        num = imagens.length - 1;
+    }
+    img_fundo.src = imagens[num]
+}
 
+function posterior() {
+    num++;
+    if (num >= imagens.length) {
+        num = 0;
+    }
+    img_fundo.src = imagens[num];
 }
